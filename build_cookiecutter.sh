3e5d6ef71e00638a8a21ffcf3bfa6dad343604a0
Shabinesh Sivaraj <ssivaraj@connectria.com>
2024-06-14 16:23:47 +0530

    update build script

--- build_cookiecutter.sh
+++ build_cookiecutter.sh
@@ -3,7 +3,15 @@
 rm transcription go.mod go.sum
 
 # to be run from main branch
+git branch -D main
+git switch -c main
 git rebase dev
 
-find . -not -path './build_cookiecutter.sh' -type f -print0 | xargs -0 sed -i '' 's/transcription/{{cookiecutter.package_slug}}/g'
-find . -not -path './build_cookiecutter.sh' -type f -print0 | xargs -0 sed -i '' 's/shabinesh/{{cookiecutter.gh_username}}/g'
+
+find . -not -path './build_cookiecutter.sh' -not -path './cookiecutter.json' -type f -print0 | xargs -0 sed -i '' 's/transcription/{{cookiecutter.package_slug}}/g'
+find . -not -path './build_cookiecutter.sh' -not -path './cookiecutter.json' -type f -print0 | xargs -0 sed -i '' 's/shabinesh/{{cookiecutter.gh_username}}/g'
+
+last_commit_message=$(git log -1 --pretty=%B <branch-name>)
+
+git add .
+git commit -m "$last_commit_message"
