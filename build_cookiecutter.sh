92f2a81d2c1bceac4b6a755052876c26d438ce0c
Shabinesh Sivaraj <ssivaraj@connectria.com>
2024-06-14 19:26:23 +0530

    update script

--- build_cookiecutter.sh
+++ build_cookiecutter.sh
@@ -8,10 +8,10 @@ git switch -c main
 git rebase dev
 
 
-find . -not -path './build_cookiecutter.sh' -not -path './cookiecutter.json' -type f -print0 | xargs -0 sed -i '' 's/transcription/{{cookiecutter.package_slug}}/g'
-find . -not -path './build_cookiecutter.sh' -not -path './cookiecutter.json' -type f -print0 | xargs -0 sed -i '' 's/shabinesh/{{cookiecutter.gh_username}}/g'
+find . -not -path './build_cookiecutter.sh' -not -path './cookiecutter.json' -not -path './test.db' -not -path './.git' -type f -print0 | xargs -0 sed -i '' 's/transcription/{{cookiecutter.package_slug}}/g'
+find . -not -path './build_cookiecutter.sh' -not -path './cookiecutter.json' -not -path './test.db' -not -path './.git' -type f -print0 | xargs -0 sed -i '' 's/shabinesh/{{cookiecutter.gh_username}}/g'
 
-last_commit_message=$(git log -1 --pretty=%B <branch-name>)
+last_commit_message=$(git log -1 --pretty=%B dev)
 
 git add .
 git commit -m "$last_commit_message"
